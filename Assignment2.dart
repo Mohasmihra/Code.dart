296b64e09f550d094a9564584d7fdcba64ee95ec
Mohas Moudy <160270015+Mohasmihra@users.noreply.github.com>
2024-03-15 14:46:20 +0300

    Add files via upload

---
+++ Assignment2.dart
@@ -0,0 +1,25 @@
+id main() {
+  // Integers
+  int age = 20; // Example of an integer data type
+  print('Age: $age');
+
+  // Doubles
+  double height = 1.85; // Example of a double data type
+  print('Height: $height meters');
+
+  // Strings
+  String name = 'Mohas'; // Example of a string data type
+  print('Name: $name');
+
+  // Lists
+  List<int> numbers = [1, 2, 3, 4, 5]; // Example of a list data type
+  print('Numbers: $numbers');
+
+  // Maps
+  Map<String, dynamic> person = {
+    'name': 'Mihra',
+    'age': 18,
+    'city': 'Arusha city'
+  }; // Example of a map data type
+  print('Person: $person');
+}
